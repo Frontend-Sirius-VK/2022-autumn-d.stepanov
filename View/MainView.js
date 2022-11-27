a8dd5440ba13ff27bc3e56ded020a01d279fd587
Denchik89 <den.stepanov.032@gmail.com>
2022-11-27 20:30:33 +0300

    clear MainView

--- View/MainView.js
+++ View/MainView.js
@@ -18,18 +18,5 @@ export class MainView {
         container.append(header);
         root.append(container);
         this.header.render(header);
-
-        // const columnLeftElement = document.createElement('div');
-        // columnLeftElement.classList.add('column-left');
-        // this.columnLeft = new ColumnLeft();
-
-        // const columnRightElement = document.createElement('div');
-        // columnRightElement.classList.add('column-right');
-        // this.columnRight = new ColumnRight();
-
-        // container.append(columnLeftElement, columnRightElement);
-        // root.append(container);
-        // this.columnLeft.render(columnLeftElement);
-        // this.columnRight.render(columnRightElement);
     }
 }
