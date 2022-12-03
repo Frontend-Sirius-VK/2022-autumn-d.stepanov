eaa040353a76867b52a4661516519f89dd0d38ce
Denchik89 <den.stepanov.032@gmail.com>
2022-12-03 11:20:01 +0300

    fix AnimeContent

--- Model/AnimeContents.js
+++ Model/AnimeContents.js
@@ -1,7 +1,7 @@
 import EventBus from "../Utils/eventBus.js";
 
 export class AnimeContents {
-    constructor(urlImage = '../Image/AnimeContent/NejnUkus.jpg', nameAnime = 'название аниме', categoryAnime = 'категория аниме', ageAnime = 1990, descriptionAnime = 'описание аниме') {
+    constructor(urlImage = '', nameAnime = 'название аниме', categoryAnime = 'категория аниме', ageAnime = 0, descriptionAnime = 'описание аниме') {
         this.urlImage = urlImage;
         this.nameAnime = nameAnime;
         this.categoryAnume = categoryAnime;
