5684e5b79e14a062e3c79e1c02ce5e8ede22fb4a
Denchik89 <den.stepanov.032@gmail.com>
2022-12-06 14:20:36 +0300

    add homework3

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ require('dotenv').config()
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser");
 const app = express();
 
-const db = require('./Model/querys.js');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('.'));
 
 const port = process.env.PORT || 3002;
+
+const db = require('./Model/querys.js');
 
 
 app.get('/', (req, res) => {
@@ -21,6 +24,29 @@ app.get('/animeContents', async (req, res) => {
     const result = await db.getAllContent();
     res.json(result);
 });
+
+
+app.post('/create', async (req, res) => {
+    const { urlImage, urlAnime, nameAnime, categoryAnime, ageAnime, descriptionAnime } = req.body;
+    const id = await db.create(urlImage, urlAnime, nameAnime, categoryAnime, ageAnime, descriptionAnime);
+
+    res.json({id});
+})
+
+app.put('/update', async (req, res) => {
+    const { id, urlImage, urlAnime, nameAnime, categoryAnime, ageAnime, descriptionAnime } = req.body;
+    const updateId = await db.update(id, urlImage, urlAnime, nameAnime, categoryAnime, ageAnime, descriptionAnime);
+
+    res.json({updateId});
+})
+
+
+app.delete('/delete', async (req, res) => {
+    const { id } = req.body;
+    const deleteId = await db.deleteContent(id);
+
+    res.json({deleteId});
+})
 
 app.get('/login', (req, res) => {
     res.send('user login');
